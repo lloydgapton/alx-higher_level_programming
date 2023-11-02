d390adb1acb89e25808160b745fa93d50c965b23
lloydgapton <lloydgapton123@gmail.com>
2023-11-02 01:22:42 -0700

    added new executable files

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """my add function
+    Args:
+        a: first integer
+        b: second int
+    Returns:
+    The return value. a + b
+    """
+    return (a + b)
