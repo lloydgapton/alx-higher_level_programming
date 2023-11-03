0379965b7b5a1a0c2c4eab63e2908753d2d4bf95
lloydgapton <lloydgapton123@gmail.com>
2023-11-03 03:33:58 -0700

    added calculator_1.py file

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My subtraction function
+    Args:
+    a:first integer
+    b:second integer
+
+    Returns:
+    The return value, a * b
+    """
+    return int(a - b)
+
+def mul(a, b):
+    """My multiplaiucation function
+
+    Args:
+
+    a: first integer
+    b: second integer
+
+    Return:
+    the return value . a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """My division function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a / b
+    """
+    return int(a / b)
