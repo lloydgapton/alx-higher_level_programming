1e2f0d035cb1b80bd3b30dba2f280006bb53eb66
lloydgapton <lloydgapton123@gmail.com>
2023-11-06 15:21:43 -0800

    updated file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return(my_list)
-    if idx >= len(my_list):
-        return(my_list)
-    new_list = list(my_list)
+    """replace element in list without modifying the org list
+
+    Args:
+        my_list: the list
+        idx: the index
+        element: the element to modify
+
+    Returns:
+        new_list or org_list if idx < 0 or
+        out of range
+    """
+    if (idx < 0 or idx > (len(my_list) - 1)):
+        return (my_list.copy())
+    new_list = my_list.copy()
     new_list[idx] = element
-    return(new_list)
-    return(my_list)
+    return (new_list)
