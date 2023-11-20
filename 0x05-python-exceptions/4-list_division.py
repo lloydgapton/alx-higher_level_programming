eeec8b97a863f7d5616d8ad2fb21b309c7f7a5ff
lloydgapton <lloydgapton123@gmail.com>
2023-11-20 13:21:34 -0800

    updated file

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    """list division  length
+
+    Args:
+        my_list_1: the first list
+        my_list_2: the seond list
+        list_length: the list length they want us to talk about
+
+    Returns:
+        return the new list
+    """
+    new_list = []
     div_list = []
     for i in range(0, list_length):
         try:
