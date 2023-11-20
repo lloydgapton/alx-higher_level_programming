080b8ca9e495adce5f8d7a91b4db25ba3faf851c
lloydgapton <lloydgapton123@gmail.com>
2023-11-20 13:16:13 -0800

    updated file

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    """safe print list integers
+
+    Args:
+        my_list: the list
+        x: the number of element to print
+
+    Return:
+        the real number of integer printed
+    """
     try:
         result = a / b
     except ZeroDivisionError:
