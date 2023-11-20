313ae4843e23321b827d7b25c24ac74ea0d93814
lloydgapton <lloydgapton123@gmail.com>
2023-11-20 12:46:25 -0800

    updated filke

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -8,6 +8,6 @@ def safe_print_integer(value):
     """
     try:
         print("{:d}".format(value))
-        return True
+        return (True)
     except:
-        return False
+        return (False)
