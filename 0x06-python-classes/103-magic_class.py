9bc2592be138779a96e7fcb839d046cf213282c5
lloydgapton <lloydgapton123@gmail.com>
2023-11-21 02:51:11 -0800

    byte Code

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,33 @@
+import math
+
+class MagicClass:
+    def __init__(self, radius=0):
+        """
+        Initializes a MagicClass instance with an optional radius.
+
+        Parameters:
+        - radius: The radius of the circle (private instance attribute).
+                  Default value is 0.
+        """
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+
+        self.__radius = radius
+
+    def area(self):
+        """
+        Computes and returns the area of the circle.
+
+        Returns:
+        - The area of the circle (pi * radius^2).
+        """
+        return math.pi * self.__radius ** 2
+
+    def circumference(self):
+        """
+        Computes and returns the circumference of the circle.
+
+        Returns:
+        - The circumference of the circle (2 * pi * radius).
+        """
+        return 2 * math.pi * self.__radius
