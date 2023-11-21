fd7661e269f14da0e1da8b46a019c22194a4531c
lloydgapton <lloydgapton123@gmail.com>
2023-11-21 02:13:18 -0800

    added a new file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size):
+        """
+        Initializes a square instance
+        Args:
+        -size:The size of the square(private instance attribute)
+        """
+        self.__size = size
