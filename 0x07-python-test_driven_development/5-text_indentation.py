209d86ffc8099ff5db77721149ba9d4c20e8af2b
lloydgapton <lloydgapton123@gmail.com>
2023-11-30 22:09:06 -0800

    text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,30 +1,23 @@
-
 #!/usr/bin/python3
-"""Defines a text-indentation function."""
+"""This is  the "text_indentation" function.
+The text_indentation function formating text. For example,
+>>> text_indentation(Non autem hoc: igitur ne illud)
+"""
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
-
-    Args:
-        text (string): The text to print.
-    Raises:
-        TypeError: If text is not a string.
-    """
-    if not isinstance(text, str):
+    """This function formating text.
+    Return the result in new text.
+    Raise Error if data is diferrent."""
+    if type(text) is not str:
         raise TypeError("text must be a string")
-
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
-
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
-            continue
-        c += 1
+    x = 0
+    check = False
+    for i in range(len(text)):
+        if text[i] in [".", ":", "?"]:
+            print(text[x : i + 1])
+            print()
+            x = i + 2
+            bool = True
+    if i + 1 == len(text) and bool is False:
+        print(text)
