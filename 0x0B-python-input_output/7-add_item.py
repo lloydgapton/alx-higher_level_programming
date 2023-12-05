55e8598feee840d3c6bfc418598fd3b59f733e98
lloydgapton <lloydgapton123@gmail.com>
2023-12-05 12:23:22 -0800

    updated file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""add all argumenst to a json file"""
 
-import json
 
+if __name__ == "__main__":
+    import sys
 
-def save_to_json_file(my_obj, filename):
-    """function that writes an Object to a text file, using a JSON"""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
+    loader = __import__('6-load_from_json_file').load_from_json_file
+    saver = __import__('5-save_to_json_file').save_to_json_file
+    try:
+        items = loader('add_item.json')
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    saver(items, 'add_item.json')
