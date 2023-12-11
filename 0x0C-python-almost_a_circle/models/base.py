da68832653adae391e899c87ef14ecb42edc5026
lloydgapton <lloydgapton123@gmail.com>
2023-12-11 14:42:08 -0800

    updated file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,114 +1,104 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-My base.py
-"""
+"""writing the base class"""
 import json
+import csv
 
 
 class Base:
-    """
-    Class Base
+    """writing the base class of this model"""
 
-    Attributes:
-        nb_objects (int): Private class attribute
-    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """
-        Class constructor
+        """constructor method
 
-        Attributes:
-            id (int): An integer input for id
+        Args:
+            id : the id
         """
-        if id is None:
+        if id is not None:
+            self.id = id
+        else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        else:
-            self.id = id
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """
-        Returns a json string representation
-
-        Attributes:
-            list_dictionaries (json): An inputted jason list of dictionaries
-        Return:
-            A json repreentation
-        """
-        if list_dictionaries is None or not list_dictionaries:
-            return '[]'
-        return json.dumps(list_dictionaries)
+        """list of dictionary to json string"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """
-        Writes the json string repressentation list object to a file
-
-        Attribute:
-            list_objs (list): object list to save
-
-        Return:
-            json object to save in file
-        """
-        file_name = cls.__name__ + ".json"
-        string = []
-        if list_objs is not None:
-            for i in list_objs:
-                string.append(cls.to_dictionary(i))
-        with open(file_name, 'w', encoding='utf-8') as file:
-            file.write(cls.to_json_string(string))
+        """save the instance of the obj to file"""
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as myfile:
+            if (list_objs is None or list_objs == []):
+                myfile.write('[]')
+            else:
+                list_dict = [i.to_dictionary() for i in list_objs]
+                myfile.write(Base.to_json_string(list_dict))
 
     @staticmethod
     def from_json_string(json_string):
-        """
-        Returns the list of json string representation
-
-        Attribute:
-            json_string (string): json object
-
-        Return:
-            json object to dictionary
-        """
-        if json_string is None or len(json_string) == 0:
+        """load list from json string"""
+        if json_string is None or json_string == "[]":
             return []
-        return json.loads(json_string)
+        else:
+            return (json.loads(json_string))
 
     @classmethod
     def create(cls, **dictionary):
-        """
-        Returns an instance with all attributes already set
-
-        Attribute:
-            dictionary (dict): inooutted dictionary
-
-        Return:
-            An instance with all attributer already set
-        """
-        if cls.__name__ == "Rectangle":
-            dummy = cls(1, 1)
-        if cls.__name__ == "Square":
-            dummy = cls(1)
-        dummy.update(**dictionary)
-        return dummy
+        """create the class method"""
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return (new)
 
     @classmethod
     def load_from_file(cls):
-        """
-        Returns a list of instances
-
-        Return:
-            A list of instances
-        """
-        file_name = cls.__name__ + ".json"
-        json_obj = []
+        """load from file"""
+        filename = str(cls.__name__) + ".json"
         try:
-            with open(file_name, 'r', encoding='utf-8') as file:
-                json_obj = cls.from_json_string(file.read())
-            for key, value in enumerate(json_obj):
-                json_obj[key] = cls.create(**json_obj[key])
-        except:
-            pass
-        return json_obj
+            with open(filename, "r") as myfile:
+                list_dicts = Base.from_json_string(myfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """serialize from csv"""
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline="") as csvfile:
+            if list_objs is None or list_objs == []:
+                csvfile.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """load from the csv file"""
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [dict([k, int(v)] for k, v in d.items())
+                              for d in list_dicts]
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
