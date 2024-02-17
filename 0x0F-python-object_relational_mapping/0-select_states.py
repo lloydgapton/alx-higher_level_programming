c0643b1b813a445cf6c0ced0c879d25047acebb3
lloydgapton <lloydgapton123@gmail.com>
2024-02-16 21:56:52 -0800

    fget states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
-"""Conect DataBase and query"""
-
+""" lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[2],
-                         passwd=sys.argv[1],
-                         db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-
-    # Execute the query
-    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC;")
-    for row in cur.fetchall():
-        print(row)
-
+    cur.execute("SELECT * FROM `states`")
+    for i in cur.fetchall():
+        print(i)
     cur.close()
     db.close()
