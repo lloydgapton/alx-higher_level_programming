81347e9bbc748dc605c6d8ca5d6cf0fd24caeff3
lloydgapton <lloydgapton123@gmail.com>
2024-02-16 22:15:56 -0800

    updated file

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,35 +1,20 @@
 #!/usr/bin/python3
-"""
-Created on Sat Aug  8 09:05:11 2020
-
-@author: Robinson Montes
-"""
-from model_state import Base, State
+""" lists all State objects from the database hbtn_0e_6_usa"""
+from model_state import State
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
 import sys
 
-
-if __name__ == '__main__':
-    args = sys.argv
-    if len(args) != 5:
-        print("Usage: {} username password database_name".format(args[0]))
-        exit(1)
-    username = args[1]
-    password = args[2]
-    data = args[3]
-    state_name = args[4]
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(username, password, data))
-    # create custom session object class from database engine
-    Session = sessionmaker(bind=engine)
-    # create instance of new custom session class
-    session = Session()
-    states = session.query(State).filter(State.name == state_name)\
-                    .order_by(State.id)
-    if states is not None and states.count() > 0:
-        for state in states:
-            print('{}'.format(state.id))
-    else:
-        print('Not found')
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State.id).filter(State.name == sys.argv[4]).first()
+    if query is None:
+        print('Not found')
+    else:
+        print(query[0])
