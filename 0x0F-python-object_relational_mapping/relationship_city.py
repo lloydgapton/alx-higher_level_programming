691c670e34ac12a7c226cb2b59699e22226503fb
lloydgapton <lloydgapton123@gmail.com>
2024-02-16 22:33:53 -0800

    made file executable

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""
-Created on Sat Aug  8 09:05:11 2020
-
-@author: Robinson Montes
-"""
-import sqlalchemy
-from sqlalchemy import Column, Integer, String, ForeignKey
+"""adv 101"""
 from relationship_state import Base, State
+from sqlalchemy import ForeignKey, Column, Integer, String
 
 
 class City(Base):
-    """
-    A new table into de data base for city representation
-    """
-    __tablename__ = "cities"
+    """The city class for this table"""
+    __tablename__ = 'cities'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""
-Created on Sat Aug  8 09:05:11 2020
-
-@author: Robinson Montes
-"""
-import sqlalchemy
-from sqlalchemy import Column, Integer, String
+"""Module for states table"""
+from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+
 
 Base = declarative_base()
 
 
 class State(Base):
-    """
-    A new table into de data base for state representation
-    """
+    """A class for state"""
     __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    cities = relationship("City", backref="state", cascade="all, delete")
