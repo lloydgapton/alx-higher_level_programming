ddc463eb97785daf54a801220146493e3754d34e
lloydgapton <lloydgapton123@gmail.com>
2024-02-16 22:49:43 -0800

    link class to database

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Created on Sat Aug  8 09:05:11 2020
-
-@author: Robinson Montes
+"""Start link class to table in database
 """
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
