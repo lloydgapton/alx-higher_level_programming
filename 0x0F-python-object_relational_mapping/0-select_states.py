10076fea3b316a5abdc6c05b60527dabff995daf
lloydgapton <lloydgapton123@gmail.com>
2024-02-16 21:53:36 -0800

    get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug  8 09:05:11 2020
+"""Conect DataBase and query"""
 
-@author: Robinson Montes
-"""
 import MySQLdb
 import sys
 
-
-if __name__ == '__main__':
-    args = sys.argv
-    if len(args) != 4:
-        print("Usage: {} username password database_name".format(args[0]))
-        exit(1)
-    username = args[1]
-    password = args[2]
-    data = args[3]
-    db = MySQLdb.connect(host='localhost', user=username,
-                         passwd=password, db=data, port=3306)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[2],
+                         passwd=sys.argv[1],
+                         db=sys.argv[3])
     cur = db.cursor()
-    num_rows = cur.execute('SELECT * FROM states ORDER BY states.id;')
-    rows = cur.fetchall()
-    for row in rows:
+
+    # Execute the query
+    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC;")
+    for row in cur.fetchall():
         print(row)
+
     cur.close()
     db.close()
