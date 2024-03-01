ab4b5eda6d44f81c75b447210ec10fde48e58b76
lloydgapton <lloydgapton123@gmail.com>
2024-03-01 15:26:11 -0800

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+""" get the status of the link with urllib"""
+
+if __name__ == '__main__':
+        import urllib.request
+
+            with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+                        content = r.read()
+                                print('Body response:')
+                                        print('\t- type: {}'.format(type(content)))
+                                                print('\t- content: {}'.format(content))
+                                                        print('\t- utf8 content: {}'.format(content.decode('utf-8')))
