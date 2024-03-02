86e20a5994994b61e688120d3fed7d6a417be4fe
lloydgapton <lloydgapton123@gmail.com>
2024-03-01 23:42:19 -0800

    update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """send a post requestwith mail"""
+from urllib import request, parse
+from sys import argv
 
 if __name__ == "__main__":
-    import urllib.request
-    import urllib.parse
-    import sys
-
-    lis = sys.argv
-    values = {}
-    values['email'] = lis[2]
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
-    req = urllib.request.Request(lis[1], data)
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8'))
+    values = {'email': argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode("ascii")
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
