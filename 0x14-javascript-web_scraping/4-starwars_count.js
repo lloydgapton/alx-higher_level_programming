e66ae58207a49cd9ebfac2bf743c5c295fc3017c
lloydgapton <lloydgapton123@gmail.com>
2024-03-26 15:03:08 -0700

    update

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*  Script that prints the number of movies
+where the character “Wedge Antilles” is present. */
+const request = require('request');
+request.get({ url: process.argv[2], json: true }, (err, res, body) => {
+  if (err) console.log(err);
+  const resultsList = body.results;
+  let characterCount = 0;
+  for (const index in resultsList) {
+    const peoplesApiList = resultsList[index].characters;
+    for (const idx in peoplesApiList) {
+      if (peoplesApiList[idx].match('/18/')) {
+        characterCount += 1;
+      }
+    }
+  }
+  console.log(characterCount);
+});
