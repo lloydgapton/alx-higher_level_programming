6e096429ddbceb9dc3b523c7039402f564765e91
lloydgapton <lloydgapton123@gmail.com>
2024-03-28 06:40:00 -0700

    updated file

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,13 +1,26 @@
 #!/usr/bin/node
-// script that prints all characters of a Star Wars movie:
+/*
+ * Script that prints all characters of a Star Wars Movie
+ *
+*/
+const movieId = process.argv[2];
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get({ url: url, json: true }, (err, res, body) => {
-  if (err) console.log(err);
-  body.characters.forEach(element => {
-    request.get({ url: element, json: true }, (err, res) => {
-      if (err) console.log(err);
-      console.log(res.body.name);
-    });
-  });
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (let i = 0; i < characters.length; i++) {
+      request.get(characters[i], (err, res, body) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const name = JSON.parse(body).name;
+          console.log(name);
+        }
+      });
+    }
+  }
 });
